58628cbdf79608a50a94156a89346378c2ec1da7
Faith <milkgirl96@yahoo.com>
2019-08-24 00:54:10 -0500

    stuff

--- src/Components/Summoners.js
+++ src/Components/Summoners.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './Summoner.css'
 
 console.log("render");
 
     const Summoners = ({ summoners }) => {
       return (
-        <div>
+        <div className="header">
           <center><h1>Summoner</h1></center>
-            
             <div className="card">
               <div className="card-body">
+               <img src="img/blade2.png" class="rotate" width="150" height="150" />
                 <img src={'http://ddragon.leagueoflegends.com/cdn/9.16.1/img/profileicon/'+summoners.profileIconId +'.png'}/>
                 <h5 className="card-title">Name: {summoners.name}</h5>
                 <h5 className="card-title">Level: {summoners.summonerLevel}</h5>
