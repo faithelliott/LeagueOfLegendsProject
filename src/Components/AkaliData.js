c1bf28944b058f09a5e01647190d92ba9f0ea85f
faithelliott <faith.ann.elliott@ndsu.edu>
2019-12-03 16:30:16 -0600

    instant summ search, added mastery icons

--- src/Components/AkaliData.js
+++ src/Components/AkaliData.js
@@ -7,21 +7,43 @@ console.log("render");
 
  
 
-    const AkaliData = ({ akalidata }) => {
+const AkaliData = ({ akalidata }) => {
   
+var level = akalidata.championLevel;
+var imgurl = 'img\\mastery7.png';
+if(level == 7){
+  imgurl = 'img\\mastery7.png'
+  console.log(imgurl)
+}else if (level ==6){
+  imgurl ='img\\mastery6.png'
+}else if (level ==5){
+  imgurl ='img\\mastery5.png'
+}else if(level == 4){
+  imgurl ='img\\mastery4.png'
+}else if(level == 3){
+  imgurl ='img\\mastery3.png'
+}else if (level ==2){
+  imgurl ='img\\mastery2.png'
+}else if(level ==1){
+  imgurl ='img\\mastery1.png'
+}else if (level ==0){
+  imgurl ='img\\mastery1.png'
+}
 
 
       return (
         <div>
           <p>
             <Card>
+           
+              <img src={imgurl}/>
                 <div className="card-text">
                   <h5 className="card-title">Mastery Level: {akalidata.championLevel}</h5>
                   {console.log(akalidata.championLevel)}
                   <h5 className="card-title">Points: {akalidata.championPoints}</h5>
                 </div>
              </Card>
-             {/*<img src="img/blade2.png" className="rotate" width="150" height="150"></img> */}</p> 
+          </p> 
         </div>
        
       )
