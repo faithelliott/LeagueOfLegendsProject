dff682f646d748417e3524ea6d345c38ce8b9599
faithelliott <faith.ann.elliott@ndsu.edu>
2019-12-22 00:48:47 -0600

    cleaned up some UI stuff, made splash screen shorter

--- src/Components/MatchHistory.js
+++ src/Components/MatchHistory.js
@@ -3,24 +3,17 @@ import './MatchHistory.css';
 import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 
-  const MatchHistory = ({ history }) => {
-    const Match=[];
-    // var count = -1;
-    // var kills=0;
+  
+const MatchHistory =  ({ history }) => {
+  const Match=[];
 
       return (
         <div>
           <p>
            {history.map( hist => (
-              axios.get('https://polar-hollows-37538.herokuapp.com/matchdata/'+hist.gameId).then(response => (Match.push(response.data.platformId))),
-              console.log('get:'+axios.get('https://polar-hollows-37538.herokuapp.com/matchdata/'+hist.gameId).then(response =>(Match.push(response.data.mapId)))),
-              // console.log(Match),
-              // count+=1,
-              // kills="under construction",
-              // console.log('count:'+count),
-         
+            axios.get('https://polar-hollows-37538.herokuapp.com/matchdata/'+hist.gameId).then(response => (Match.push(response.data.platformId))),
             <Card>
-              <img src={'img\\'+hist.lane+'.png'}></img>
+              <img id="sizing" src={'img\\'+hist.lane+'.png'}></img>
               <li key={hist.gameId}>
               <h5 className="card-title1">Lane:{hist.lane}</h5> 
               <h5 className="card-title1">Role:{hist.role}</h5> 
