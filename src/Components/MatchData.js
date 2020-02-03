03205adf4c519f68b35581336746e06adeded3d7
faithelliott <faith.ann.elliott@ndsu.edu>
2020-02-02 21:07:03 -0600

    updating

--- src/Components/MatchData.js
+++ src/Components/MatchData.js
@@ -4,29 +4,32 @@ import Card from 'react-bootstrap/Card';
 
 const MatchData = ({ datas }) => {
   return (
-        <div>    
+        <div className="m4">    
             <Card>
               <h5 className="card-title">Full Match data test Pull...</h5>
               <h5 className="card-title">Mode: {datas.gameMode}</h5>
               <h5 className="card-title">Duration: {datas.gameDuration}</h5>
               <h5 className="card-title">Type: {datas.gameType}</h5>
             </Card>  
+           <div className="card2">  
             {datas.teams && datas.teams.length && datas.teams.map(data => (   
-                 <Card>         
+                 <Card>     
                   <h5 className="card-title">TeamID: {data.teamId}</h5>
                   <h5 className="card-title">Win: {data.win}</h5>
                   <h5 className="card-title">Towers: {data.towerKills}</h5>
                   <h5 className="card-title">Dragons: {data.dragonKills}</h5>
                  </Card>
                  ))} 
+                 </div>
+             <div className="card2">
               {datas.participants && datas.participants.length && datas.participants.map(dat => (   
-                  <Card>         
-                      <h5 className="card-title">Player: {dat.participantId}</h5>
-                      <h5 className="card-title">Kills: {dat['stats'].kills}</h5>
-                      <h5 className="card-title">Deaths: {dat['stats'].deaths}</h5>
-                      <h5 className="card-title">Assits: {dat['stats'].assists}</h5>
+                  <Card>
+                      <h5>Player: {dat.participantId}</h5>
+                      <h5>Kills: {dat['stats'].kills}</h5>
+                      <h5>Deaths: {dat['stats'].deaths}</h5>
+                      <h5>Assits: {dat['stats'].assists}</h5>
                   </Card>
-                 ))} 
+                 ))} </div>
       </div> 
    )
 }
